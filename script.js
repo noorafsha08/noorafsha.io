34373e3c9c1da8c88c0bb4fb71997a9d37ee65e1
noorafsha08 <afshanoor0812@gmail.com>
2024-09-11 03:49:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling effect for the navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
